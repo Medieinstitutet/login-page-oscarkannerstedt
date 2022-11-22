e7b52ef4a5ab0aa1875b18dc2f5de55552dfd7c5
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2022-11-22 16:09:04 +0100

    lagt till text på sidan inloggad eller fel inlogg

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const userName = document.getElementById("userName");
 const Password = document.getElementById("password");
 
 const user = [
-    { userName: 'janne', Password: 'test'}
+    { userName: "janne", Password: "test"}
 ]
 
 
@@ -11,28 +11,14 @@ loginBtn.addEventListener('click', () => {
     console.log('klick på knapp');
     //console.log("userName", userName);
 if( user[0].Password === Password.value && user[0].userName === userName.value) {
-    console.log('inloggad');
+    //console.log('inloggad');
+    demo.innerHTML = "Du är inloggad som: " + user.userName;
     } else {
-        console.log('fel använarnamn eller lösenord');
+        //console.log('fel använarnamn eller lösenord');
+        demo.innerHTML = "fel användarnamn eller lösenord"; 
     }
 });
 
-
-/*document.body.onload = addElement;
-function addElement() {
-    // create a new div element
-    const newDiv = document.createElement("div");
-  
-    // and give it some content
-    const newContent = document.createTextNode("Hi there and greetings!");
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-  }*/
 
 
 //for ( i = 0; i < user.length; i++)
