dc6855b224aaa91a95c0adffd0c4ca02c13e8992
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2022-11-22 18:58:06 +0100

    börjat med local storage

--- script.js
+++ script.js
@@ -11,13 +11,28 @@ loginBtn.addEventListener('click', () => {
     console.log('klick på knapp');
     //console.log("userName", userName);
 if( user[0].Password === Password.value && user[0].userName === userName.value) {
-    //console.log('inloggad');
-    demo.innerHTML = "Du är inloggad som: " + user.userName;
+    
+    //Spara användare i localstorage
+    console.log("user i Event Listener", user);
+    localStorage.setItem("user", user);
+    printName();
     } else {
+        PrintUnknow();
         //console.log('fel använarnamn eller lösenord');
-        demo.innerHTML = "fel användarnamn eller lösenord"; 
+        //demo.innerHTML = "fel användarnamn eller lösenord"; 
     }
 });
+
+//Hämta namnet från Ls och skriv ut på sidan
+function printName() {
+    let user = localStorage.getItem("user");
+    demo.innerText = "Välkommen " + userName.value;
+}
+
+//Användaren finns inte
+function PrintUnknow() {
+    demo.innerText = "Fel användarnamn eller lösenord";
+}
 
 
 
